4a2d97ea53ea3dbede39104f1b9c52b59df021c4
EuanEdgar <euan@spydr.com>
2025-12-01 13:36:37 +0000

    Comment Problem to make it more clear which methods can be implemented or overwritten

--- src/problem.rb
+++ src/problem.rb
@@ -1,14 +1,20 @@
 class Problem
   def self.parse_class_name(class_name)
-    result = /Day(?<day>\d+)::(?:Part)(?<part>\d+)/.match(class_name)
-    raise "Invalid class name: #{class_name}" unless result
-    result.named_captures.transform_values(&:to_i).transform_keys(&:to_sym)
+    @class_name_parse_result ||= begin
+      result = /Day(?<day>\d+)::(?:Part)(?<part>\d+)/.match(class_name)
+      raise "Invalid class name: #{class_name}" unless result
+      result.named_captures.transform_values(&:to_i).transform_keys(&:to_sym)
+    end
   end
 
   def self.inherited(subclass)
+    # Validate that class name fits the expected format
     parse_class_name(subclass.name)
   end
 
+  # Load and parse the input file
+  # Define `self.parse_input` to customize the parsing
+  # Otherwise, the raw string is returned
   def self.load_input
     input = File.read(file_path(part: self.part, example: @load_example)).chomp
     if respond_to?(:parse_input)
@@ -35,6 +41,9 @@ class Problem
     caller_location = caller_locations.find { |p| /day_\d+\/part_\d+/ =~ p.to_s }
     File.expand_path(path, File.dirname(caller_location.path))
   end
+
+  # Override this method to change the path to the input file
+  # By default, part is unused, but you can use it to differentiate between input files for different parts
   def self.path_for(part:, example: false)
     if example
       "./example_input.txt"
@@ -46,6 +55,11 @@ class Problem
   def self.call(example: false)
     @load_example = example
     self.run
+  end
+
+  # Override this method to implement the solution
+  def self.run
+    raise NotImplementedError
   end
 
   def self.day
